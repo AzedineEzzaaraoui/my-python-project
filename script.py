9566e69985124ab2283b6b13c77cf038e2af068b
Azedine <azedine.zaaraoui@gmail.com>
2025-02-09 15:36:41 +0100

    Modification du message de test dans le script

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import datetime
 def main():
     print("Hello from GitHub Actions!")
     print(f"Current date and time: {datetime.datetime.now()}")
+    print("Test message: This is a modification.")
 
 if __name__ == "__main__":
     main()
